d9b9be140172849999e33d223dae7f81c4ccb184
Wisher7274 <3449558612@qq.com>
2026-09-17 00:19:52 +0800

    day2: 1-9 counting, using pointer array

--- main.c
+++ main.c
@@ -1,26 +1,17 @@
-#include <stdio.h>
 #include <unistd.h>
-
 int main(void) {
-	printf("Sorry i can't write in C well yet.\n");
-
-	const char *BEGIN = "\r";
-	char *frames[4] = {
-		"|",
-		"/",
-		"-",
-		"\\",
-	};
-	
-	for(int i = 0;;) {
-		write(1 , BEGIN , 1);
-		write(1 , frames[i] , 1);
-		if(i<3) {
-			i++;
-		} else {
-			i = 0;
-		}
-		usleep(100000);
-	}
-	return 0;
+  const char *CR = "\r";
+  char *frames[10];
+  char ch[] = {'0','1','2','3','4','5','6','7','8','9'};
+  
+  for (int i = 0;i < 10; i++) {
+    frames[i] = &ch[i];
+  }
+  for (int i = 0;;i++) {
+    if (10 == i) {i = 0;}
+    write(1,CR,1);
+    write(1,frames[i],1);
+    usleep(100000);
+  }
+  return 0;
 }
