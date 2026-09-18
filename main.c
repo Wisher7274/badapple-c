db24ee78c083f62089356fb8e4565cd1f00ba812
Wisher7274 <3449558612@qq.com>
2026-09-18 23:59:00 +0800

    day3: buncing bull-xy-ver

--- main.c
+++ main.c
@@ -1,17 +1,35 @@
 #include <unistd.h>
-int main(void) {
-  const char *CR = "\r";
-  char *frames[10];
-  char ch[] = {'0','1','2','3','4','5','6','7','8','9'};
-  
-  for (int i = 0;i < 10; i++) {
-    frames[i] = &ch[i];
-  }
-  for (int i = 0;;i++) {
-    if (10 == i) {i = 0;}
-    write(1,CR,1);
-    write(1,frames[i],1);
+#include <stdio.h>
+#include <string.h>
+struct vec2 {
+  int x, y;
+};
+struct ball {
+  struct vec2 pos; 
+  struct vec2 velocity;
+  struct vec2 edge;
+};
+int main(int argc, char* argv[]) {
+  const char* CLR = "\033[2J\033[H";
+  struct ball b = {
+    .pos = { 0 , 0 },
+    .velocity = { 0 , 1 },
+    .edge = { 10 , 10 },
+  };
+  for (;;) {
+    if (b.pos.x == b.edge.x) {
+      b.velocity.x = -b.velocity.x;
+    }
+    if (b.pos.y == b.edge.y) {
+      b.velocity.y = -b.velocity.y;
+    }
+    b.pos.x += b.velocity.x;
+    b.pos.y += b.velocity.y;
+    char hint[50];
+    sprintf(hint, "X: %d, Y: %d", b.pos.x, b.pos.y);
+    printf("%s\n", hint);
     usleep(100000);
   }
   return 0;
 }
+
